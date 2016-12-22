26dbdb36f457696b677681a8f96fff8aef7837cb
Meceral <Siegele.Dominik@gmx.at>
2016-12-22 16:01:49 +0100

    Added DirectCalculationSimplifier => does what the Simplifier => Expression now in Property of Simplifier

--- Calculator/Calculator.Logic.Tests/SimplifierTests.cs
+++ Calculator/Calculator.Logic.Tests/SimplifierTests.cs
@@ -13,8 +13,7 @@ namespace Calculator.Logic.Tests
         {
             var tokens = Tokenize(input);
             var inputTree = CreateInMemoryModel(tokens);
-            var underTest= new Simplifier(inputTree);
-            var simplified = underTest.Simplify();
+            var simplified = Simplifier.Simplify(inputTree);
             var asString = new FormattingExpressionVisitor().Format(simplified);
             asString.Should().Be(expected);
         }
@@ -26,75 +25,5 @@ namespace Calculator.Logic.Tests
             return tokens;
         }
         static IExpression CreateInMemoryModel(IEnumerable<IToken> tokens) => new ModelBuilder().BuildFrom(tokens);
-        [Test]
-        public void Simplification_Of_Nested_Additions()
-        {
-            Check("2+2+2+2a", "6 + 2*a");
-        }
-        [Test]
-        public void Simplification_Of_ParenthesedExpression()
-        {
-            Check("2a+(3+2)", "2*a + 5");
-        }
-        [Test]
-        public void Simplification_Without_Variables()
-        {
-            Check("2-2", "0");
-        }
-        [Test]
-        public void Simplify_Does_Not_Change_Input_Expression_Tree()
-        {
-            var input = CreateInMemoryModel(Tokenize("2+2+2+2a"));
-            var underTest= new Simplifier(input);
-            underTest.Simplify();
-            ((Addition) input).Left.Should().BeOfType<Addition>();
-        }
-
-        [Test]
-        public void Simplification_Of_Nested_Operations_On_Both_Sides_Of_Variables()
-        {
-            Check("1+2+3+4+5a+6+7+8+9a", "10 + 5*a + 21 + 9*a");
-        }
-
-        [Test]
-        public void Simplification_Of_Parentheses_Next_To_Variables()
-        {
-            Check("(1*2/2+3-4)-2a+(1*2/2+3-4a +5*6)", "0 - 2*a + (4 - 4*a + 30)");
-        }
-
-        [Test]
-        public void Simplification_Works_With_Multiplication()
-        {
-            Check("1*2*3-4a+5*6*7", "6 - 4*a + 210");
-        }
-        [Test]
-        public void Simplification_Works_With_Division()
-        {
-            Check("3/2-4a+4/4/2", "1.5 - 4*a + 0.5");
-        }
-
-        [Test]
-        public void Simplification_Works_With_Parentheses()
-        {
-            Check("(1+2)*3+4a+5*(6+7)", "9 + 4*a + 65");
-        }
-
-        [Test]
-        public void Simplification_Of_Expression_Where_Variable_Is_First_Element()
-        {
-            Check("5a+3+4", "5*a + 7");
-        }
-
-        [Test]
-        public void Simplification_Of_Constants_in_Parentheses()
-        {
-            Check("(3)+2a", "3 + 2*a");
-        }
-
-        [Test]
-        public void Simplifier_Works_As_Calculator()
-        {
-            Check("1+2-3+5*6/6", "5");
-        }
     }
 }
--- Calculator/Calculator.Logic/Simplifier.cs
+++ Calculator/Calculator.Logic/Simplifier.cs
@@ -4,143 +4,49 @@ namespace Calculator.Logic
 {
     public class Simplifier : IExpressionVisitor
     {
-        IExpression mExpression;
-
-        public Simplifier(IExpression expression)
+        public static IExpression DirectCalculationExpression { get; set; }
+        public static IExpression OriginalExpression { get; set; }
+        public static IExpression Simplify(IExpression input)
         {
-            mExpression = ExpressionCloner.Clone(expression);
-        }
-
-        public Simplifier() {}
-
-        public void Visit(Subtraction subtraction)
-        {
-            CalculateResultIfPossible(subtraction);
-        }
-
-        public void Visit(Multiplication multiplication)
-        {
-            CalculateResultIfPossible(multiplication);
-        }
-
-        public void Visit(Addition addition)
-        {
-            CalculateResultIfPossible(addition);
-        }
-
-        public void Visit(Division division)
-        {
-            CalculateResultIfPossible(division);
+            var directSimplifier = DirectCalculationSimplifier.Simplify(input);
+            OriginalExpression = input;
+            DirectCalculationExpression = directSimplifier;
+            return directSimplifier;
         }
 
         public void Visit(ParenthesedExpression parenthesed)
         {
-            if (parenthesed.Wrapped is IArithmeticOperation)
-                CalculateResultIfPossible((IArithmeticOperation) parenthesed.Wrapped);
+            throw new System.NotImplementedException();
         }
 
-        public void Visit(Constant constant) {}
-
-        public void Visit(Variable variable) {}
-
-        public static IExpression Simplify(IExpression input)
+        public void Visit(Subtraction subtraction)
         {
-            var simplifier = new Simplifier(input);
-            return simplifier.Simplify();
+            throw new System.NotImplementedException();
         }
 
-        public IExpression Simplify()
+        public void Visit(Multiplication multiplication)
         {
-            mExpression.Accept(this);
-            return mExpression;
+            throw new System.NotImplementedException();
         }
 
-        static bool IsCalculateable(IArithmeticOperation operation)
-            => operation.Left is Constant && operation.Right is Constant;
-
-        void CalculateResultIfPossible(IArithmeticOperation operation)
+        public void Visit(Addition addition)
         {
-            LoopthroughVisitors(operation);
-            if (operation.Left is ParenthesedExpression)
-                HandleParenthesesExpressionOnTheLeftSide(operation);
-            else if (operation.Right is ParenthesedExpression)
-                HandleParenthesesExpressionOnTheRightSide(operation);
-            if (IsCalculateable(operation))
-            {
-                var constant = new Constant {Value = EvaluatingExpressionVisitor.Evaluate(operation)};
-                if (operation.Parent != null)
-                    HandleOperationWithParent(operation, constant);
-                else
-                    mExpression = constant;
-            }
-            else if ((operation.Left is Addition || operation.Left is Subtraction) && operation.Right is Constant)
-                CalculateRightHandAdditionAndSubtractions(operation);
+            throw new System.NotImplementedException();
         }
 
-        static void HandleOperationWithParent(IArithmeticOperation operation, Constant constant)
+        public void Visit(Constant constant)
         {
-            if (operation.Parent is IArithmeticOperation)
-                HandleParentAsArithmeticOperation(operation, constant);
-            else
-            {
-                var parent = (ParenthesedExpression) operation.Parent;
-                parent.Wrapped = constant;
-            }
+            throw new System.NotImplementedException();
         }
 
-        static void HandleParentAsArithmeticOperation(IArithmeticOperation operation, Constant constant)
+        public void Visit(Division division)
         {
-            var parent = (IArithmeticOperation) operation.Parent;
-            if (parent.Left == operation)
-                parent.Left = constant;
-            else
-                parent.Right = constant;
+            throw new System.NotImplementedException();
         }
 
-        void CalculateRightHandAdditionAndSubtractions(IArithmeticOperation operation)
+        public void Visit(Variable variable)
         {
-            var operationLeft = (IArithmeticOperation) operation.Left;
-            if (operationLeft.Right is Constant)
-            {
-                if (operation.Parent != null)
-                {
-                    var parent = (IArithmeticOperation) operation.Parent;
-                    parent.Left = operationLeft;
-                }
-                operation.Left.Parent = operation.Parent;
-                operation.Left = operationLeft.Right;
-                var constant = new Constant {Value = EvaluatingExpressionVisitor.Evaluate(operation)};
-                operationLeft.Right = constant;
-                operationLeft.Right.Parent = operationLeft;
-                if (operation.Parent == null)
-                {
-                    mExpression = operationLeft;
-                }
-            }
-        }
-
-        static void HandleParenthesesExpressionOnTheLeftSide(IArithmeticOperation operation)
-        {
-            var parenthesis = (ParenthesedExpression) operation.Left;
-            if (parenthesis.Wrapped is Constant)
-            {
-                operation.Left = parenthesis.Wrapped;
-            }
-        }
-
-        static void HandleParenthesesExpressionOnTheRightSide(IArithmeticOperation operation)
-        {
-            var parenthesis = (ParenthesedExpression) operation.Right;
-            if (parenthesis.Wrapped is Constant)
-            {
-                operation.Right = parenthesis.Wrapped;
-            }
-        }
-
-        void LoopthroughVisitors(IArithmeticOperation operation)
-        {
-            operation.Left.Accept(this);
-            operation.Right.Accept(this);
+            throw new System.NotImplementedException();
         }
     }
 }
