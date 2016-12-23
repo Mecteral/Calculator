54b8569fc60370cc316df7a982015a3976abd4a8
max <max@nix>
2016-12-23 01:00:15 +0100

    added test project for Calculator.Model

---
+++ Calculator/Calculator.Model.Tests/AnArithmeticOperationTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Model.Tests
+{
+    [TestFixture(typeof(Addition))]
+    [TestFixture(typeof(Subtraction))]
+    [TestFixture(typeof(Multiplication))]
+    [TestFixture(typeof(Division))]
+    public class AnArithmeticOperationTests<T> where T: AnArithmeticOperation, new()
+    {
+        
+    }
+}
