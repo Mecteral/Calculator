8bcdf9f566b1029c3295202b712f66d89aeef3a6
max <max@nix>
2016-12-23 01:01:59 +0100

    test coverage for AnArithmethicOperation

--- Calculator/Calculator.Model.Tests/AnArithmeticOperationTests.cs
+++ Calculator/Calculator.Model.Tests/AnArithmeticOperationTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace Calculator.Model.Tests
 {
@@ -6,8 +7,19 @@ namespace Calculator.Model.Tests
     [TestFixture(typeof(Subtraction))]
     [TestFixture(typeof(Multiplication))]
     [TestFixture(typeof(Division))]
-    public class AnArithmeticOperationTests<T> where T: AnArithmeticOperation, new()
+    public class AnArithmeticOperationTests<T> where T : AnArithmeticOperation, new()
     {
-        
+        [Test]
+        public void Setting_Left_Sets_Parent_Of_Value()
+        {
+            var underTest = new T {Left = new Constant()};
+            underTest.Left.Parent.Should().BeSameAs(underTest);
+        }
+        [Test]
+        public void Setting_Right_Sets_Parent_Of_Value()
+        {
+            var underTest = new T {Right = new Constant()};
+            underTest.Right.Parent.Should().BeSameAs(underTest);
+        }
     }
 }
