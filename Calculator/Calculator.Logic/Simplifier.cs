f47a708e30a62bd0b3077476a0f63a72e419d1ad
Mecteral <Siegele.Dominik@gmx.at>
2017-01-02 23:49:02 +0100

    Added Enumerable for all possible simplifications

--- Calculator/Calculator.Logic/Simplifier.cs
+++ Calculator/Calculator.Logic/Simplifier.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Calculator.Model;
 
 namespace Calculator.Logic
 {
     public class Simplifier : ISimplifier
     {
+        IEnumerable<IExpression> AllPossibleSimplifications = new List<IExpression>();
         static IExpression SimplifiedCalculationExpression { get; set; }
         static IExpression DirectCalculationExpression { get; set; }
         static IExpression OriginalExpression { get; set; }
