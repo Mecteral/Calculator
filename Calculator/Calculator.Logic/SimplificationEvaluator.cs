c0aa3784a24bff55e4382f2446976e25c165240d
Mecteral <Siegele.Dominik@gmx.at>
2017-01-03 22:39:12 +0100

    Added Class SimplificationEvaluator

---
+++ Calculator/Calculator.Logic/SimplificationEvaluator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Model;
+
+namespace Calculator.Logic
+{
+    public class SimplificationEvaluator : IExpressionVisitor
+    {
+        int mExpressionCount;
+        public IExpression FindSmallesExpressionInEnumerable(IEnumerable<IExpression> expressions)
+        {
+            if (expressions == null)
+                return null;
+            var result = expressions.First();
+            foreach (var expression in expressions)
+            {
+                var currentExpression = expression;
+                if (CountExpression(expression) < CountExpression(result))
+                {
+                    result = currentExpression;
+                }
+            }
+            return result;
+        }
+
+        int CountExpression(IExpression expression)
+        {
+            mExpressionCount = 0;
+            expression.Accept(this);
+            return mExpressionCount;
+        }
+        public void Visit(ParenthesedExpression parenthesed)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Subtraction subtraction)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Multiplication multiplication)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Addition addition)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Constant constant)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Division division)
+        {
+            mExpressionCount++;
+        }
+
+        public void Visit(Variable variable)
+        {
+            mExpressionCount++;
+        }
+    }
+}
