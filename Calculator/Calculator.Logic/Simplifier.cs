bcec63cb0fd93ef212c35d1e418b5c2c9bed21a8
max <max@nix>
2017-01-11 01:52:49 +0100

    refactored

--- Calculator/Calculator.Logic/Simplifier.cs
+++ Calculator/Calculator.Logic/Simplifier.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Calculator.Logic.Model;
 using Calculator.Model;
@@ -8,37 +7,33 @@ namespace Calculator.Logic
 {
     public class Simplifier : ISimplifier
     {
+        IEnumerable<ISimplifier> Simplifiers
+        {
+            get
+            {
+                yield return new DirectCalculationSimplifier();
+                yield return new ParenthesesSimplifier();
+                yield return new AdditionAndSubtractionMover();
+                yield return new VariableCalculator();
+            }
+        }
         public IExpression Simplify(IExpression input)
         {
             var equalityChecker = new ExpressionEqualityChecker();
-            var mover = new AdditionAndSubtractionMover();
-            var parenthesesSimplifier = new ParenthesesSimplifier();
-            var variableCalculator = new VariableCalculator();
-            var directCalculator = new DirectCalculationSimplifier();
+
             bool hasChanged;
             var lastStep = ExpressionCloner.Clone(input);
             do
             {
-                var transformed = directCalculator.Simplify(lastStep);
-                Console.WriteLine(UseFormattingExpressionVisitor(transformed));
-
-                transformed = parenthesesSimplifier.Simplify(transformed);
-                Console.WriteLine(UseFormattingExpressionVisitor(transformed));
-
-                transformed = mover.Simplify(transformed);
-                Console.WriteLine(UseFormattingExpressionVisitor(transformed));
-
-                transformed = variableCalculator.Simplify(transformed);
-                Console.WriteLine(UseFormattingExpressionVisitor(transformed));
+                var transformed = Simplifiers.Aggregate(lastStep, (current, simplifier) => simplifier.Simplify(current));
 
                 hasChanged = !equalityChecker.IsEqual(lastStep, transformed);
-                if (hasChanged)
-                {
-                    lastStep = transformed;
-                }
-            } while (hasChanged);
+                if (hasChanged) { lastStep = transformed; }
+            }
+            while (hasChanged);
             return lastStep;
         }
-        static string UseFormattingExpressionVisitor(IExpression expression) => new FormattingExpressionVisitor().Format(expression);
+        static string UseFormattingExpressionVisitor(IExpression expression)
+            => new FormattingExpressionVisitor().Format(expression);
     }
 }
