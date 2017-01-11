10b65179231ddd20278c28ed4204ec74bb922e51
max <max@nix>
2017-01-11 04:39:35 +0100

    somewhat tested

--- Calculator/Calculator.Logic.Tests/DoubleDispatchTests.cs
+++ Calculator/Calculator.Logic.Tests/DoubleDispatchTests.cs
@@ -1,4 +1,5 @@
 ﻿using Calculator.Model;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Calculator.Logic.Tests
@@ -7,14 +8,32 @@ namespace Calculator.Logic.Tests
     public class DoubleDispatchTests
     {
         [Test]
-        public void Usage_01()
+        public void If_No_Fitting_Handler_Defined_Calls_FallbackHandler()
         {
             IExpression left = new Constant();
             IExpression right = new Constant();
-            var dispatcher = new Dispatcher(left, right) { FallbackHandler = (l, r) => { } };
-            dispatcher.OnLeft<Constant>().OnRight<Variable>().Do((c, v) => { });
-            dispatcher.OnLeft<Addition>().OnRight<Multiplication>().Do((a, m) => { });
+            var wasFallbackCalled = false;
+            var dispatcher = new Dispatcher(left, right) {FallbackHandler = (l, r) => wasFallbackCalled = true};
+            dispatcher.OnLeft<Constant>().OnRight<Variable>().Do((c, v) => { Assert.Fail(); });
+            dispatcher.OnLeft<Addition>().OnRight<Constant>().Do((a, m) => { Assert.Fail(); });
             dispatcher.Dispatch();
+
+            wasFallbackCalled.Should().BeTrue();
+        }
+        [Test]
+        public void Picks_Right_Handler_If_Defined()
+        {
+            IExpression left = new Addition();
+            IExpression right = new Constant();
+            var dispatcher = new Dispatcher(left, right) {FallbackHandler = (l, r) => Assert.Fail()};
+
+            dispatcher.OnLeft<Constant>().OnRight<Variable>().Do((c, v) => { Assert.Fail(); });
+            var wasRightComboPicked = false;
+            dispatcher.OnLeft<Addition>().OnRight<Constant>().Do((a, m) => { wasRightComboPicked = true; });
+
+            dispatcher.Dispatch();
+
+            wasRightComboPicked.Should().BeTrue();
         }
     }
 }
