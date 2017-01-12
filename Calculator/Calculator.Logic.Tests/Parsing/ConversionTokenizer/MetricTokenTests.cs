06c3d9698ee5c42ce4d25dba4efc11a43db455f8
Mecteral <Siegele.Dominik@gmx.at>
2017-01-12 01:00:13 +0100

    Added Test TG-77 #closed

--- Calculator/Calculator.Logic.Tests/Parsing/ConversionTokenizer/MetricTokenTests.cs
+++ Calculator/Calculator.Logic.Tests/Parsing/ConversionTokenizer/MetricTokenTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator.Logic.Parsing.CalculationTokenizer;
 using NUnit.Framework.Internal;
 using NUnit.Framework;
 using Calculator.Logic.Parsing.ConversionTokenizer;
@@ -41,6 +42,17 @@ namespace Calculator.Logic.Tests.Parsing.ConversionTokenizer
         public void MeterStaysMeter()
         {
             CheckWithNumber("1000m", 0, 1000);
+        }
+
+        [Test]
+        public void MetricUnitsWithAddition()
+        {
+            var converter = new Logic.Parsing.ConversionTokenizer.ConversionTokenizer();
+            converter.Tokenize("10m+20km");
+            var underTest = converter.Tokens;
+            underTest.ElementAt(0).Should().BeOfType<MetricToken>().Which.Value.Should().Be(10);
+            underTest.ElementAt(1).Should().BeOfType<OperatorToken>().Which.Operator.Should().Be(Operator.Add);
+            underTest.ElementAt(2).Should().BeOfType<MetricToken>().Which.Value.Should().Be(20000);
         }
     }
 }
