71fdb658a6e5c576c303825ea545a1454f0b594e
Mecteral <Siegele.Dominik@gmx.at>
2017-01-17 22:58:08 +0100

    Added TokenFormatter tests

--- Calculator/Calculator.Logic.Tests/TokenFormatterTests.cs
+++ Calculator/Calculator.Logic.Tests/TokenFormatterTests.cs
@@ -11,12 +11,12 @@ namespace Calculator.Logic.Tests
         [Test]
         public void TokenFormatter_Converts_Tokens_Into_Proper_Signs()
         {
-            var input = "(2+3)*4/5-6.7";
+            var input = "(2+3)*4/5-6.7+cos(60deg)+sin(30deg)+tan(45deg)+2a";
             var tokenizer = new Tokenizer();
             tokenizer.Tokenize(input);
             var underTest = new TokenFormatter();
             var result = underTest.Format(tokenizer.Tokens);
-            result.Should().Be("(2+3)*4/5-6.7");
+            result.Should().Be("(2+3)*4/5-6.7+0.5+0.5+1.0+2*a");
         }
     }
 }
