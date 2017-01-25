9bd907591711df3c550d4887526c920310f3a6ea
Mecteral <Siegele.Dominik@gmx.at>
2017-01-25 21:02:07 +0100

    Added Errorcases to FileReader

--- Calculator/Calculator.Logic/CommandLineParser/ConfigFileReader.cs
+++ Calculator/Calculator.Logic/CommandLineParser/ConfigFileReader.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.IO;
 using System.Linq;
+using Calculator.Logic.Parsing.ConversionTokenizer;
 using Microsoft.VisualBasic.ApplicationServices;
 using ModernRonin.PraeterArtem.Functional;
 
@@ -11,21 +12,25 @@ namespace Calculator.Logic.CommandLineParser
     {
         const string Path = @"C:\Users\Public\Calc\ConfigFileCalculator.txt";
         static readonly List<string> sReceivers = new List<string>();
+        static readonly List<string> sValues = new List<string>();
         public static string[] ReadFile()
         {
             if (File.Exists(Path))
             {
                 var config = File.ReadAllLines(Path);
-                ExtractReceivers(config);
+                ExtractReceiversAndValues(config);
                 CheckForInvalidArguments();
+                CheckForInvalidEntries();
                 return config;
             }
             return null;
         }
 
-        static void ExtractReceivers(IEnumerable<string> config)
+        static void ExtractReceiversAndValues(IEnumerable<string> config)
         {
-            var temp = "";
+            var tempReceiver = "";
+            var tempValue = "";
+            var valueStart = 0;
             foreach (var s in config)
             {
                 var start = 0;
@@ -45,15 +50,57 @@ namespace Calculator.Logic.CommandLineParser
                 {
                     if (char.IsLetter(s[i]))
                     {
-                        temp += s[i];
+                        tempReceiver += s[i];
                     }
                     else if (s[i] == '=' || s[i] == '+' || s[i] == '-')
                     {
+                        valueStart = i;
                         break;
                     }
                 }
-                sReceivers.Add(temp);
-                temp = "";
+                sReceivers.Add(tempReceiver);
+                tempReceiver = "";
+                for (var i = valueStart; i < s.Length; i++)
+                {
+                    if (s[i] != '\"' && s[i] != '=')
+                    {
+                        tempValue += s[i];
+                    }
+
+                }
+                sValues.Add(tempValue);
+                tempValue = "";
+            }
+        }
+
+        static void CheckForInvalidEntries()
+        {
+            string usedUnit = null;
+            string usedRad = null;
+            var abbreviation = new UnitAbbreviations();
+            var fieldValues = abbreviation.GetType()
+                .GetFields()
+                .Where(fi => fi.IsLiteral && !fi.IsInitOnly)
+                .Select(field => field.GetValue(null))
+                .ToList();
+            for (var i = 0; i < sReceivers.Count; i++)
+            {
+                if (sReceivers[i] == "u" || sReceivers[i] == "unit")
+                {
+                    if (usedUnit == null)
+                        usedUnit = sValues[i];
+                    if (usedUnit != null && sValues[i] != usedUnit)
+                        throw new InvalidExpressionException("Conflicting entries with unit or u");
+                    if (!fieldValues.Contains(usedUnit))
+                        throw new InvalidExpressionException("Wrong abbreviation in config file");
+                }
+                else if (sReceivers[i] == "d" || sReceivers[i] == "degree")
+                {
+                    if (usedRad == null)
+                        usedRad = sValues[i];
+                    if (usedRad != null && sValues[i] != usedRad)
+                        throw new InvalidExpressionException("Conflicting entries with degree or d");
+                }
             }
         }
         static void CheckForInvalidArguments()
@@ -66,9 +113,8 @@ namespace Calculator.Logic.CommandLineParser
             .ToList();
             if (sReceivers.Select(s => fieldValues.Select(fieldValue => fieldValue.ToString()).Any(toTest => s == toTest)).Any(isEqual => !isEqual))
             {
-                throw new InvalidExpressionException();
+                throw new InvalidExpressionException("There is an Argument defined in the config file that doesnt exist.");
             }
-
         }
     }
 }
