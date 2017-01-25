db5f0efbb4fe63fc0074ce866745c86e23784fe9
Mecteral <Siegele.Dominik@gmx.at>
2017-01-25 17:39:30 +0100

    Fixed bug

--- Calculator/Calculator.Logic/ArgumentParsing/ApplicationArguments.cs
+++ Calculator/Calculator.Logic/ArgumentParsing/ApplicationArguments.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator.Logic.ArgumentParsing
+﻿namespace Calculator.Logic.ArgumentParsing
 {
     public class ApplicationArguments
     {
--- Calculator/Calculator.Logic/CommandLineParser/CommandLineParserCreator.cs
+++ Calculator/Calculator.Logic/CommandLineParser/CommandLineParserCreator.cs
@@ -15,8 +15,8 @@ namespace Calculator.Logic.CommandLineParser
         {
             var help = new HelpText();
             var parser = new FluentCommandLineParser();
-            parser.Setup<bool>(ParserShortAndLongNames.ToDegreeShort, ParserShortAndLongNames.ToDegreeLong).Callback(arg => args.ToDegree = arg).SetDefault(false);
-            parser.Setup<string>(ParserShortAndLongNames.UnitConversionShort, ParserShortAndLongNames.UnitConversionLong).Callback(arg => args.UnitForConversion = arg).SetDefault(null);
+            parser.Setup<bool>(ParserShortAndLongNames.ToDegreeShort, ParserShortAndLongNames.ToDegreeLong).Callback(arg => args.ToDegree = arg);
+            parser.Setup<string>(ParserShortAndLongNames.UnitConversionShort, ParserShortAndLongNames.UnitConversionLong).Callback(arg => args.UnitForConversion = arg);
             parser.SetupHelp(ParserShortAndLongNames.HelpShort, ParserShortAndLongNames.HelpLong).Callback(text => Console.WriteLine(help.HelpStrings.Aggregate("", (current, s) => current + s)));
             return parser;
         }
