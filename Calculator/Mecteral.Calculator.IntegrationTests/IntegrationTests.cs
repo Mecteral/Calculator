8367a32e3c51f565183e1d96333a16bc7d9453c6
max <max@nix>
2017-03-30 21:30:14 +0200

    easier to write integration tests for simplification pipeline

---
+++ Calculator/Mecteral.Calculator.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Calculator.Logic;
+using Calculator.Logic.ArgumentParsing;
+using Calculator.Logic.Pipelines;
+using FluentAssertions;
+using Mecteral.Calculator.IntegrationTests.Properties;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Mecteral.Calculator.IntegrationTests
+{
+    [TestFixture]
+    // ReSharper disable once TestFileNameWarning
+    public class IntegrationTests
+    {
+        static IEnumerable<string[]> TestCases
+        {
+            get
+            {
+                return
+                    Resources.TestCases.Split('\r', '\n')
+                        .Where(s => !string.IsNullOrWhiteSpace(s))
+                        .Select(l => l.Split('='));
+            }
+        }
+        //[TestCase("1+1", "2")]
+        [TestCaseSource(nameof(TestCases))]
+        public void Run_Integration_Case(string input, string expected)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(typeof(ContainerModule).Assembly);
+            var container = builder.Build();
+            var simplificationPipeline = container.Resolve<ISimplificationPipeline>();
+            simplificationPipeline.UseSimplificationPipeline(input, Substitute.For<IApplicationArguments>())
+                .Should()
+                .Be(expected);
+        }
+    }
+}
