9c98c5b9c9a5959a42c85e969d6f8236245c8d07
max <max@nix>
2017-03-31 20:24:17 +0200

    added StringExtensions.WithoutAnyWhitespace()

---
+++ Calculator/Calculator.Logic/Utilities/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Calculator.Logic.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string WithoutAnyWhitespace(this string self)
+        {
+            return Regex.Replace(self, @"\s+", string.Empty);
+        }
+    }
+}
