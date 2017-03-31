2712cd60a3438937aca5ac6ac8ac2a8478275441
max <max@nix>
2017-03-31 21:51:53 +0200

    introduced ATraversingReplacer

--- Calculator/Calculator.Logic/Simplifying/NeutralElementEliminatingSimplifier.cs
+++ Calculator/Calculator.Logic/Simplifying/NeutralElementEliminatingSimplifier.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using Calculator.Logic.Model;
 using Calculator.Model;
+using ModernRonin.PraeterArtem.Functional;
 
 namespace Calculator.Logic.Simplifying
 {
@@ -10,6 +13,33 @@ namespace Calculator.Logic.Simplifying
             if (null == multiplication) return input;
             // TODO: do the same for division, and for addition/subtraction with zero
             return new NeutralElementOfMultiplicationRemover(multiplication).Transform();
+        }
+    }
+
+    public abstract class ATraversingReplacer : ISimplifier
+    {
+        IExpression mResult;
+        public IExpression Simplify(IExpression input)
+        {
+            mResult = ExpressionCloner.Clone(input);
+            Traverse(input);
+            return mResult;
+        }
+        protected abstract IExpression Replace(IExpression expression);
+        void Traverse(IExpression original)
+        {
+            var replacement = Replace(original);
+            if (replacement != original)
+            {
+                if (!original.HasParent) mResult = replacement;
+                else original.Parent.ReplaceChild(original, replacement);
+            }
+            else
+             GetChildren(original).UseIn(Traverse); 
+        }
+        IEnumerable<IExpression> GetChildren(IExpression expression)
+        {
+            yield break;
         }
     }
 }
