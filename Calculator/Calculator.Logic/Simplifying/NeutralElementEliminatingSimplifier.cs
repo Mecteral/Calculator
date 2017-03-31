159a1dd8f074c252acc01164b4c21e8a74e2409e
max <max@nix>
2017-03-31 22:46:04 +0200

    added all new neutral element removers to NeutralElementEliminatingSimplifier

--- Calculator/Calculator.Logic/Simplifying/NeutralElementEliminatingSimplifier.cs
+++ Calculator/Calculator.Logic/Simplifying/NeutralElementEliminatingSimplifier.cs
@@ -4,10 +4,13 @@ namespace Calculator.Logic.Simplifying
 {
     public class NeutralElementEliminatingSimplifier : AVisitingTraversingReplacer
     {
-        // TODO: do the same for division, and for addition/subtraction with zero
         protected override IExpression ReplaceMultiplication(Multiplication multiplication)
-        {
-            return new NeutralElementOfMultiplicationRemover(multiplication).Transform();
-        }
+            => new NeutralElementOfMultiplicationRemover(multiplication).Transform();
+        protected override IExpression ReplaceSubtraction(Subtraction subtraction)
+            => new NeutralElementOfSubtractionRemover(subtraction).Transform();
+        protected override IExpression ReplaceAddition(Addition addition)
+            => new NeutralElementOfAdditionRemover(addition).Transform();
+        protected override IExpression ReplaceDivision(Division division)
+            => new NeutralElementOfDivisionRemover(division).Transform();
     }
 }
