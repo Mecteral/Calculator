d4f37219bc6358fea0fcde64eb935be83eaa0a70
max <max@nix>
2017-03-31 22:44:01 +0200

    added: * AZeroBasedNeutralElementOfArithmeticOperationRemover<> * NeutralElementOfAdditionRemover * NeutralElementOfSubtractionRemover

--- Calculator/Calculator.Logic/Simplifying/NeutralElementOfMultiplicationRemover.cs
+++ Calculator/Calculator.Logic/Simplifying/NeutralElementOfMultiplicationRemover.cs
@@ -4,17 +4,36 @@ using November.MultiDispatch;
 
 namespace Calculator.Logic.Simplifying
 {
-    public class AOneBasedNeutralElementOfArithmeticOperationRemover<T> :
-        ANeutralElementOfArithmeticOperatorRemover<T> where T : IArithmeticOperation
+    public class AOneBasedNeutralElementOfArithmeticOperationRemover<T> : ANeutralElementOfArithmeticOperatorRemover<T>
+        where T : IArithmeticOperation
     {
         protected AOneBasedNeutralElementOfArithmeticOperationRemover(T operation) : base(operation) {}
         protected override decimal NeutralElement => 1;
     }
 
-    public class NeutralElementOfMultiplicationRemover : AOneBasedNeutralElementOfArithmeticOperationRemover<Multiplication>
+    public class AZeroBasedNeutralElementOfArithmeticOperationRemover<T> : ANeutralElementOfArithmeticOperatorRemover<T>
+        where T : IArithmeticOperation
+    {
+        protected AZeroBasedNeutralElementOfArithmeticOperationRemover(T operation) : base(operation) {}
+        protected override decimal NeutralElement => 0;
+    }
+
+    public class NeutralElementOfAdditionRemover : AZeroBasedNeutralElementOfArithmeticOperationRemover<Addition>
+    {
+        public NeutralElementOfAdditionRemover(Addition operation) : base(operation) {}
+    }
+
+    public class NeutralElementOfSubtractionRemover : AZeroBasedNeutralElementOfArithmeticOperationRemover<Subtraction>
+    {
+        public NeutralElementOfSubtractionRemover(Subtraction operation) : base(operation) {}
+    }
+
+    public class NeutralElementOfMultiplicationRemover :
+        AOneBasedNeutralElementOfArithmeticOperationRemover<Multiplication>
     {
         public NeutralElementOfMultiplicationRemover(Multiplication multiplication) : base(multiplication) {}
     }
+
     public class NeutralElementOfDivisionRemover : AOneBasedNeutralElementOfArithmeticOperationRemover<Division>
     {
         public NeutralElementOfDivisionRemover(Division operation) : base(operation) {}
