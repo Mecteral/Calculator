656dc6bcb86f80fb682187df43d6175a0d92a214
max <max@nix>
2017-04-03 20:02:26 +0200

    replaced loop with functional construct

--- Calculator/Calculator.Logic/Parsing/CalculationTokenizer/InputStringValidator.cs
+++ Calculator/Calculator.Logic/Parsing/CalculationTokenizer/InputStringValidator.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Linq;
+using ModernRonin.PraeterArtem.Functional;
 
 namespace Calculator.Logic.Parsing.CalculationTokenizer
 {
     public class InputStringValidator
     {
-        static readonly Func<char, bool>[] sCharacterClassValidators = {
-            IsConversionMarker, IsVariable, IsOperator, IsWhiteSpace, IsDigit, IsDecimal, IsParenthesis
+        static readonly Func<char, bool>[] sCharacterClassValidators =
+        {
+            IsConversionMarker, IsVariable, IsOperator,
+            IsWhiteSpace, IsDigit, IsDecimal, IsParenthesis
         };
         int mFunctionEnd;
         string mInput;
-
         public void Validate(string input)
         {
             mInput = input;
@@ -18,18 +20,10 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
             CheckParanthesesCount();
             CheckForDoubleVariableAndFunction();
         }
-
-        void CheckForUnknownCharacters()
+        void CheckForUnknownCharacters() => mInput.UseIn(ValidateCharacter);
+        static void ValidateCharacter(char c)
         {
-            for (var i = 0; i < mInput.Length; i++)
-            {
-                var c = mInput[i];
-                if (
-                    !IsValidCharacter(c))
-                {
-                    throw new CalculationException("Unknown character in input", i);
-                }
-            }
+            if (!IsValidCharacter(c)) throw new CalculationException("Unknown character in input", c);
         }
         static bool IsValidCharacter(char c) => sCharacterClassValidators.Any(v => v(c));
         static bool IsConversionMarker(char c) => c == '=' || c == '?';
@@ -51,28 +45,17 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
                     count++;
                     lastOpeningParenthesesIndex = i;
                 }
-                else if (c == ')')
-                {
-                    count--;
-                }
+                else if (c == ')') { count--; }
             }
-            if (count != 0)
-            {
-                throw new CalculationException("Uneven Parenthesescount", lastOpeningParenthesesIndex);
-            }
+            if (count != 0) { throw new CalculationException("Uneven Parenthesescount", lastOpeningParenthesesIndex); }
         }
-
         string ExtractFuntionConditionsFromString(int start)
         {
             var result = "";
-            if (mInput[start + 1] == ')')
-            {
-                throw new CalculationException("The Function has no Value", start);
-            }
+            if (mInput[start + 1] == ')') { throw new CalculationException("The Function has no Value", start); }
             for (var i = start + 1; i < mInput.Length; i++)
             {
-                if (mInput[i] != ')')
-                    result += mInput[i];
+                if (mInput[i] != ')') result += mInput[i];
                 else
                 {
                     mFunctionEnd = i;
@@ -81,7 +64,6 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
             }
             return result;
         }
-
         static int CheckTrigonometricFunctions(string input)
         {
             var numberIsOver = false;
@@ -89,10 +71,7 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
             for (var i = 0; i < input.Length; i++)
             {
                 var c = input[i];
-                if (char.IsLetter(c) && input.Length <= i + 2)
-                {
-                    return i;
-                }
+                if (char.IsLetter(c) && input.Length <= i + 2) { return i; }
                 if (!degOrRadDefined && input.Length >= i + 2 && c == 'r' && input[i + 1] == 'a' && input[i + 2] == 'd' ||
                     c == 'd' && input[i + 1] == 'e' && input[i + 2] == 'g')
                 {
@@ -101,27 +80,19 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
                     i += 2;
                 }
                 else if (!numberIsOver && (char.IsNumber(c) || c == '.' || c == ',')) {}
-                else if (degOrRadDefined)
-                {
-                    return i;
-                }
+                else if (degOrRadDefined) { return i; }
             }
             return 0;
         }
-
         static int CheckFunctionsWithValue(string input)
         {
             for (var i = 0; i < input.Length; i++)
             {
                 var c = input[i];
-                if (c != '.' && c != ',' && !char.IsNumber(c))
-                {
-                    return i;
-                }
+                if (c != '.' && c != ',' && !char.IsNumber(c)) { return i; }
             }
             return 0;
         }
-
         void CheckForDoubleVariableAndFunction()
         {
             if (mInput.Length <= 1) return;
@@ -129,13 +100,11 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
             {
                 var c = mInput[i - 1];
                 if (i + 2 < mInput.Length && c == 'c' && mInput[i] == 'o' && mInput[i + 1] == 's' &&
-                    mInput[i + 2] == '(' ||
-                    c == 's' && mInput[i] == 'i' && mInput[i + 1] == 'n' && mInput[i + 2] == '(' ||
+                    mInput[i + 2] == '(' || c == 's' && mInput[i] == 'i' && mInput[i + 1] == 'n' && mInput[i + 2] == '(' ||
                     c == 't' && mInput[i] == 'a' && mInput[i + 1] == 'n' && mInput[i + 2] == '(')
                 {
                     var errorIndex = CheckTrigonometricFunctions(ExtractFuntionConditionsFromString(i + 2));
-                    if (errorIndex != 0)
-                    {
+                    if (errorIndex != 0) {
                         throw new CalculationException("The Function was not properly defined", i + errorIndex);
                     }
                     i += mFunctionEnd;
@@ -144,14 +113,12 @@ namespace Calculator.Logic.Parsing.CalculationTokenizer
                          mInput[i + 2] == 't' && mInput[i + 3] == '(')
                 {
                     var errorIndex = CheckFunctionsWithValue(ExtractFuntionConditionsFromString(i + 3));
-                    if (errorIndex != 0)
-                    {
+                    if (errorIndex != 0) {
                         throw new CalculationException("The Function was not properly defined", i + errorIndex);
                     }
                     i += mFunctionEnd;
                 }
-                else if (char.IsLetter(c) && char.IsLetter(mInput[i]))
-                {
+                else if (char.IsLetter(c) && char.IsLetter(mInput[i])) {
                     throw new CalculationException("Variables need to be seperated with an Operator", i);
                 }
             }
