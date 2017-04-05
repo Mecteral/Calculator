37dda274e16c4bf7605b4786791c4190355432c6
Mecteral <Siegele.Dominik@gmx.at>
2017-04-05 22:25:06 +0200

    Added PowerSimplifier

---
+++ Calculator/Calculator.Logic/Simplifying/PowerSimplifier.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.Model;
+
+namespace Calculator.Logic.Simplifying
+{
+    public class PowerSimplifier : AVisitingTraversingReplacer
+    {
+        protected override IExpression ReplaceMultiplication(Multiplication multiplication)
+        {
+            if (multiplication.Left.GetConstantValue() == multiplication.Right.GetConstantValue())
+                return new Power {Left = multiplication.Left, Right = multiplication.Right};
+            return multiplication;
+        }
+    }
+}
